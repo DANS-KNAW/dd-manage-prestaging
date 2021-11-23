d6320c4eb326fe832041e5788247818586d67f6b
Jan van Mansum <janvanmansum@gmail.com>
2021-11-23 16:50:42 +0100

    Improved command line handling + add db-tables.sql script

---
+++ src/main/assembly/dist/install/db-tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS basic_file_meta (
+    storage_identifier VARCHAR(60) NOT NULL,
+    dataset_doi VARCHAR(100) NOT NULL,
+    version_sequence_number INTEGER NOT NULL,
+    file_name VARCHAR(1000) NOT NULL,
+    directory_label VARCHAR(1000),
+    mime_type VARCHAR(255) NOT NULL,
+    sha1_checksum CHAR(40) NOT NULL,
+    PRIMARY KEY (storage_identifier, dataset_doi, version_sequence_number));
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON basic_file_meta TO dd_manage_prestaging;
